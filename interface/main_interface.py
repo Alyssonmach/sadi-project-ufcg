b3270afa09287174d142467766aaacbae79ec603
Alysson Machado <alyssonmachado388@gmail.com>
2024-09-18 08:06:53 -0300

    feat: interactive version

--- interface/main_interface.py
+++ interface/main_interface.py
@@ -1,29 +1,65 @@
-import streamlit as st 
 import matplotlib.pyplot as plt
+import streamlit as st 
+import yaml
+import os
 
+# Configurando alguns parâmetros da página no navegador
 st.set_page_config(page_title = 'SADI', layout = "wide")
 
+def load_yaml(archive = 'config.yaml'):
+    '''
+    Função que carrega os dados em um dicionário a partir de um arquivo YAML.
+
+    Inputs:
+        archive (str) -> caminho local do arquivo YAML.
+    
+    Returns:
+        yaml_dict (dict) -> dicionário com as variáveis do YAML.
+    '''
+
+    with open(archive, 'r') as file:
+        yaml_dict = yaml.safe_load(file)
+    
+    return yaml_dict
+
+# Parâmetros possíveis de serem atualizados na interface
+STATUS_ANALYSIS = ['Em Espera', 'Aprovado', 'Reprovado']
+PLATE_NUMBER = ['BCV6189', 'RI02A19', 'Em Espera']
+STATUS_MOTOR = ['Fechado', 'Abrindo', 'Aberto', 'Fechando']
+IMAGES = ['car1.jpeg', 'car2.jpg', 'not-car.jpg']
+
+# Carregando os dados do arquivo YAML
+data = load_yaml()
+status_anaysis = data['status_analysis']
+status_motor = data['status_motor']
+
+# Atualizando os parâmetros atuais mostráveis na interface
+if status_anaysis == STATUS_ANALYSIS[0]: plate_number, images = PLATE_NUMBER[2], IMAGES[2]
+elif status_anaysis == STATUS_ANALYSIS[1]: plate_number, images = PLATE_NUMBER[0], IMAGES[0]
+elif status_anaysis == STATUS_ANALYSIS[2]: plate_number, images = PLATE_NUMBER[1], IMAGES[1]
+image = plt.imread(f'assets/{images}')
+
+# Cabeçalho da Página
 st.markdown('# Sistema de Estacionamento Inteligente')
 st.divider()
 
+# Personalizando a primeira parte do corpo da página (Informações de Inferências)
 col1, col2, col3 = st.columns(spec = 3)
 
-with col1:
-    image = plt.imread('assets/carro.jpeg')
-    st.image(image = image, caption = 'Imagem Capturada', width = 400)
+with col1: st.image(image = image, caption = 'Imagem Capturada', width = 400)
 
 with col2:
     col1, col2 = st.columns(spec = [4,6])
     with col1: st.text('Status de Análise: ')
-    with col2: st.warning('Analisando Placa...')
+    with col2: st.warning(status_anaysis)
     st.divider()
     col1, col2 = st.columns(spec = [4,6])
     with col1: st.text('Número da Placa: ')
-    with col2: st.warning('BCV6189')
+    with col2: st.warning(plate_number)
     st.divider()
     col1, col2 = st.columns(spec = [4,6])
     with col1: st.text('Status do Motor: ')
-    with col2: st.error('Fechado')
+    with col2: st.error(status_motor)
 
 with col3:
     st.text('Frames de Análise')
@@ -37,13 +73,13 @@ with col3:
 
 st.divider()
 
-if 'tried_login' not in st.session_state:
-    st.session_state.tried_login = False
+# Personalizando a segunda parte do corpo da página (Atualização na base de dados)
 
-if 'sucessfully_login' not in st.session_state:
-    st.session_state.sucessfully_login = False
+if 'tried_login' not in st.session_state: st.session_state.tried_login = False
+if 'sucessfully_login' not in st.session_state: st.session_state.sucessfully_login = False
 
 with st.expander('Acesso do Administrador'):
+    # Parte que deve ser mostrada caso o usuário não esteja logado no sistema
     if st.session_state.sucessfully_login == False:
         col1, col2 = st.columns(2)
         with col1: login = st.text_input(label = 'Login: ')
@@ -57,6 +93,7 @@ with st.expander('Acesso do Administrador'):
             st.session_state.tried_login = True
             if login == 'admin' and password == '1234':
                 st.session_state.sucessfully_login = True
+    # Parte mostrada quando o usuário consegue logar com sucesso na área restrita
     else:
         logoff = st.button('Sair da Área Restrita')
         if logoff:
